a41f5cd2ffc6ba85646958755a0d0ba710631584
Chau Le <chau.le@hcss.com>
2019-10-19 01:53:14 -0500

    parse output to csv file

--- main.js
+++ main.js
@@ -31,3 +31,93 @@ function check() {
 		alert("both files there!");
 	}
 };
+
+
+function convertToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+};
+
+function exportCSVFile(headers, items, fileTitle) {
+	
+	if (headers) {
+        items.unshift(headers);
+    }
+    var jsonObject = JSON.stringify(items);
+	
+    var csv = this.convertToCSV(jsonObject);
+
+    var exportedFile = fileTitle + '.csv' || 'export.csv';
+
+    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    
+	var link = document.createElement("a");
+	if (link.download !== undefined) { 
+		var url = URL.createObjectURL(blob);
+		link.setAttribute("href", url);
+		link.setAttribute("download", exportedFile);
+		link.style.visibility = 'hidden';
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
+    
+}
+
+function download(){
+  	var headers = {
+		company: 'Company',
+		student_1: 'Student Ranking #1',
+		student_2: 'Student Ranking #2',
+		student_3: 'Student Ranking #3',
+		student_4: 'Student Ranking #4',
+		student_5: 'Student Ranking #5',
+		student_6: 'Student Ranking #6',
+  	}
+
+	let dataSet = {
+		'American':  ['Brian', 'Faith', 'Jessica', 'George', 'Ian', 'Alex'],
+		'City':  ['Brian', 'Alex', 'Cassie', 'Faith', 'George', 'Dana'],
+		'County': ['Faith', 'Brian', 'Edward', 'George', 'Hannah', 'Cassie'],
+		'Fairview':  ['Faith', 'Jessica', 'Cassie', 'Alex', 'Ian', 'Hannah'],
+		'Mercy':  ['Jessica', 'Hannah', 'Faith', 'Dana', 'Alex', 'George'],
+		'Saint Mark':  ['Brian', 'Alex', 'Edward', 'Ian', 'Jessica', 'Dana'],
+		'Park':  ['Jessica', 'George', 'Hannah', 'Faith', 'Brian', 'Alex'],
+		'Deaconess': ['George', 'Jessica', 'Brian', 'Alex', 'Ian', 'Dana'],
+		'Mission':  ['Ian', 'Cassie', 'Hannah', 'George', 'Faith', 'Brian'],
+		'General':  ['Edward', 'Hannah', 'George', 'Alex', 'Brian', 'Jessica'],
+	}
+  
+
+	var output = [];
+
+  	var keys = Object.keys(dataSet);
+ 	keys.forEach(function(key){
+	  	let studentArray = dataSet[key];
+		output.push({
+			company: key,
+			student_1: studentArray[0],
+			student_2: studentArray[1],
+			student_3: studentArray[2],
+			student_4: studentArray[3],
+			student_5: studentArray[4],
+			student_6: studentArray[5],
+		});
+	})
+	console.log(output);
+  	var fileTitle = 'RankingFile'; 
+  	exportCSVFile(headers, output, fileTitle); 
+};

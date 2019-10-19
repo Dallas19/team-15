337ba49c3fe76eb4be4a2f52b57135bae387db5b
Chau Le <cle6393@gmail.com>
2019-10-19 09:34:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ function download(){
 
 	let dataSet = {
 		'JPMorgan Chase':  ['Brian', 'Faith', 'Jessica', 'George', 'Ian', 'Alex'],
-		'City':  ['Brian', 'Alex', 'Cassie', 'Faith', 'George', 'Dana'],
+		'CitiBank':  ['Brian', 'Alex', 'Cassie', 'Faith', 'George', 'Dana'],
 		'County': ['Faith', 'Brian', 'Edward', 'George', 'Hannah', 'Cassie'],
 		'Fairview':  ['Faith', 'Jessica', 'Cassie', 'Alex', 'Ian', 'Hannah'],
 		'Mercy':  ['Jessica', 'Hannah', 'Faith', 'Dana', 'Alex', 'George'],

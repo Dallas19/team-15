3f284526973c6d921c2ad424f8659cd77216a432
Ozone Kafley <kafleyozone@gmail.com>
2019-10-19 02:28:08 -0500

    not working post

--- eif-flask-server/app.py
+++ eif-flask-server/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS, cross_origin
 
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
   return "Hello World!"
+
+
+@app.route('/dict', methods=['GET'])
+@cross_origin()
+def new_dict():
+  new_dict = {}
+  new_dict.update({"Hello": "Got it"})
+  return new_dict
+
+@app.route('/upload')
+def upload():
+  return "uploaded?"
+
 
 if __name__ == '__main__':
   app.run()
--- main.js
+++ main.js
@@ -23,11 +23,47 @@ function upload(evt) {
 	}
 };
 
+function post_request(fileA, fileB) {
+	// $.ajax({
+	// 	type: "POST",
+	// 	url: "/test.py",
+	// 	success: testFunction
+	// });
+
+	// const request = async () => {
+	// 	const response = await fetch("http://127.0.0.1:5000/dict",{
+	// 		method: 'GET', // *GET, POST, PUT, DELETE, etc.
+	// 		mode: 'no-cors', // no-cors, *cors, same-origin
+	// 		credentials: 'include', // include, *same-origin, omit
+	// 	});
+	// 	const json = await response.json();
+	// 	console.log(json);
+	// }
+	
+	// request();
+	// let formData = new FormData();
+	// formData.append('name', 'John');
+	// fetch("http://127.0.0.1:5000/upload", {
+	// 	body: formData,
+	// 	method: "post"
+	// }).then(response => console.log(response));
+}
+
+function testFunction(response) {
+	console.log("in callback. The python file must have responded.");
+}
+
 function check() {
-	if (document.getElementById("fileA-input").value == "" || 
-	document.getElementById("fileB-input").value == "") {
+	var fileAinput = document.getElementById("fileA-input");
+	var fileBinput = document.getElementById("fileB-input");
+	if (fileAinput.value == "" || fileBinput.value == "") {
 		alert("Please upload both files at the same time before submitting...")
 	} else {
-		alert("both files there!");
+		console.log("both files there!");
+		// make POST request here
+		post_request(fileAinput.value, fileBinput.value);
+		// clear the files after the upload
+		fileAinput.value = "";
+		fileBinput.value = "";
 	}
 };
